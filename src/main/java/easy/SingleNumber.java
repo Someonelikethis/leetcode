19e933ac516d8c778e8043ea3245222b87c2b910
lizhanxu <zhanxuli1997@163.com>
2019-12-04 14:28:53 +0800

    fix SingleNumber

--- src/main/java/easy/SingleNumber.java
+++ src/main/java/easy/SingleNumber.java
@@ -9,33 +9,53 @@ package easy;
 public class SingleNumber {
     public static void main(String[] args) {
         int[] nums = new int[]{4, 1, 2, 1, 2};
-        System.out.println(singleNumber(nums));
+        System.out.println(forceSingleNumber(nums));
+        System.out.println(singleNumberByBit(nums));
     }
 
-    public static int singleNumber(int[] nums) {
+    //暴力解法  执行时间长   353ms   37.8MB
+    public static int forceSingleNumber(int[] nums) {
         int res = 0;
-        boolean flag = true;
         out:
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) {
+                    continue ;
+                }
                 if(nums[i]==nums[j]){
-                    flag=false;
                     continue out;
                 }
             }
-            if (flag) {
-                return nums[i];
-            }
-            res = i;
+            res=i;
         }
         return nums[res];
-//        int i=0;
-//        for (int j = i + 1; j < nums.length; j++) {
-//            if (nums[i]==nums[j]){
-//                i++;
-//                j=i;
-//            }
-//        }
-//        return nums[i];
+    }
+
+    //
+
+    /**
+     * 位操作    1ms   38MB
+     *
+     * java没有同或运算符
+     *
+     * 异或   不同为1
+     * 同或   相同为1
+     *
+     * 交换律：a ^ b ^ c <=> a ^ c ^ b
+     *
+     * 任何数与0异或为任何数 0 ^ n => n
+     *
+     * 相同的数异或为0: n ^ n => 0
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public static int singleNumberByBit(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
     }
 }
